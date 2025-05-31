ba673cad86d3846aba1482a5aaf54ca873fde216
Ankit Patidar <ankitpatidar01650@gmail.com>
2025-05-31 17:33:10 +0530

    Add Hotwire for Live Updates

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ end
 
 gem 'devise'
 gem "cssbundling-rails"
+
+gem "hotwire-rails", "~> 0.1.3"
--- app/controllers/habit_checkins_controller.rb
+++ app/controllers/habit_checkins_controller.rb
@@ -3,15 +3,22 @@ class HabitCheckinsController < ApplicationController
   before_action :set_habit
 
   def create
-    @habit.habit_checkins.create(checkin_date: Date.parse(params[:date]))
-    redirect_to habit_path(@habit)
+    @habit_checkin = @habit.habit_checkins.create(checkin_date: Date.parse(params[:date]))
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to habit_path(@habit) }
+    end
   end
-
+  
   def destroy
-    @habit.habit_checkins.find_by(checkin_date: Date.parse(params[:date]))&.destroy
-    redirect_to habit_path(@habit)
+    @habit_checkin = @habit.habit_checkins.find_by(checkin_date: Date.parse(params[:date]))
+    @habit_checkin.destroy if @habit_checkin
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to habit_path(@habit) }
+    end
   end
-
+  
   private
 
   def set_habit
